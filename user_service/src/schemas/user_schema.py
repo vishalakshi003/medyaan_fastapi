8d398d2c5580838d454d23977836adb035760c96
MuthuKumaran <smuthukumar498@gmail.com>
2025-06-10 15:51:39 +0530

    [MOD]changes

---
+++ user_service/src/schemas/user_schema.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr, UUID4
+from typing import List, Optional, Any
+from datetime import date, time, datetime
+from enum import Enum
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+    balance: Optional[float]
+    bio: Optional[str]
+    preferences: Optional[dict]
+    tags: Optional[List[str]]
+    birth_date: Optional[date]
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    is_active: bool
+
+    
