1f1ab6bd50b8a9cff5732a8269032ac22aa93cad
vishalakshi <vishalakshi.k@medyaan.com>
2025-06-10 15:44:20 +0530

    [ADD]create users models

--- user_service/src/core/database.py
+++ user_service/src/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from config import Config
+from src.core.config import Config
 
 engine = create_engine(Config.DATABASE_URL)
 
---
+++ user_service/src/models/customuser.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from sqlalchemy import ARRAY, Column, Date, DateTime,Integer,String,Float,Boolean,Text,JSON
+from ..core.database import Base
+class CustomUser(Base):
+    __tablename__ = "users"
+
+    id=Column(Integer,primary_key=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    balance = Column(Float)
+    is_active = Column(Boolean, default=True)
+    bio = Column(Text)
+    preferences = Column(JSON)
+    tags = Column(ARRAY(String))
+    birth_date = Column(Date, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
