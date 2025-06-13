b8bdf5a9fb4febf762e1924e3364c5c3a66f0c05
MuthuKumaran <smuthukumar498@gmail.com>
2025-06-13 18:07:19 +0530

    [MOD]

--- blog_service/main.py
+++ blog_service/main.py
@@ -5,7 +5,7 @@ from src.routes.user_routes import user_router
 from src.routes.rolemaster import role_router
 app=FastAPI()
 
-app.include_router(blog_router)
+app.include_router(blog_router,tags=['Blogs'])
 
-app.include_router(user_router)
-app.include_router(role_router)
+app.include_router(user_router,tags=['users'])
+app.include_router(role_router,tags=['roles'])
