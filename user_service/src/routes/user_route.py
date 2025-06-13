d5c363dd5f1fa779e080421d98127de36ef20abf
MuthuKumaran <smuthukumar498@gmail.com>
2025-06-13 10:56:34 +0530

    [MOD]changes

--- user_service/src/routes/user_route.py
+++ user_service/src/routes/user_route.py
@@ -1,9 +1,10 @@
+from sqlalchemy import Select
 from src.models.customuser import CustomUser
 from ..schemas.user_schema import *
 from fastapi import Depends,APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.core.database import get_db,async_get_db
-from sqlalchemy.orm import Session,joinedload
+from sqlalchemy.orm import Session,joinedload,selectinload
 from src.models.rolemaster import *
 user_router=APIRouter()
 @user_router.post('/create/users',status_code=201,response_model=UserResponse)
@@ -52,6 +53,21 @@ def GetRoleResponse(db:Session=Depends(get_db)):
 def get_user(db:Session=Depends(get_db)):
     
     user_details = db.query(CustomUser).options(joinedload(CustomUser.role_mappings).joinedload(RoleMapping.role)).all()
+    for user in user_details:
+        temp = []
+        for mapping in user.role_mappings:
+            temp.append(RoleResponse.from_orm(mapping.role))
+        user.roles = temp
+    return user_details
+
+@user_router.get('/get_users/async',response_model=List[UserResponse])
+async def get_user_async(db:AsyncSession=Depends(async_get_db)):
+    try:
+        result = await db.execute(Select(CustomUser).options(selectinload(CustomUser.role_mappings).selectinload(RoleMapping.role)))
+        user_details = result.scalars().all()
+    except Exception as e:
+        user_details=[]
+        print(e)
     for user in user_details:
         temp = []
         for mapping in user.role_mappings:
