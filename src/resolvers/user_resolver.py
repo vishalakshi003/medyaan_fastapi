a14f2df31f22f31b1e4b9c828d6da847f418308a
MuthuKumaran <smuthukumar498@gmail.com>
2025-06-19 14:59:12 +0530

    [MOD]changes

--- src/resolvers/user_resolver.py
+++ src/resolvers/user_resolver.py
@@ -1,10 +1,11 @@
-from schema.user_schema import CreateUserInput, UserType,LoginReq,TokenRes
+from schema.user_schema import Create_user, User_details,LoginReq,TokenRes
 from services.user_service import fetch_users, add_user,token
+from strawberry.types import Info
 
 async def get_users() -> list[UserType]:
     return await fetch_users()
 
-async def create_user(data: CreateUserInput) -> UserType:
-    return await add_user(data)
+async def create_user(data: Create_user,info:Info) -> User_details:
+    return await add_user(data,info)
 async def login_api(data:LoginReq)->TokenRes:
     return await token(data)
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,6 +1,6 @@
 from graphql import GraphQLError
 from models.user_model import CustomUser
-from schema.user_schema import UserType,TokenRes
+from schema.user_schema import User_details,Create_user,TokenRes
 from config.database import get_db
 from sqlalchemy.future import select
 from strawberry.types import Info
@@ -13,14 +13,25 @@ async def fetch_users() -> list[UserType]:
         users = result.scalars().all()
         return [UserType(id=u.id, name=u.name, email=u.email) for u in users]
 
-async def add_user(data) -> UserType:
-    async for db in get_db():
-        user = User(name=data.name, email=data.email)
-        db.add(user)
-        await db.commit()
-        await db.refresh(user)
-        return UserType(id=user.id, name=user.name, email=user.email)
-    
+async def add_user(data,info) -> User_details:
+    try:
+        db: AsyncSession = info.context["db"]
+        async with db.begin():
+            if data.password != data.password1:
+                raise GraphQLError('enter correct password',extensions={"status_code":400})
+            hash_password=password_context.hash(data.password)
+            emails_exists_res=await db.execute(select(CustomUser).where(CustomUser.email==data.email))
+            email_exists=emails_exists_res.scalar_one_or_none()
+            if email_exists:
+                raise GraphQLError('email already exists',extensions={"status_code":400})
+            
+            users=CustomUser(email=data.email,mobile_number=data.mobile_number,password=hash_password)
+            db.add(users)
+            await db.commit()
+            await db.refresh(users)
+            return users
+    except GraphQLError as gql_error:
+        raise gql_error 
     
 async def token(data,info:Info)->TokenRes:
     db:AsyncSession=info.context["db"]
